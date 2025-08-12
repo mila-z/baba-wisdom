8010428d43e2477d03462688a2ec37d32c23a1cf
mila-z <m_zaprianova@protonmail.com>
2025-08-12 14:57:20 +0300

    refactor: remove create_app function from main file and move it to api/__init__.py

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-from flask import Flask
-from models import db
-import os
-from dotenv import load_dotenv
+from api import create_app
 
-load_dotenv()
+app = create_app()
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///baba_wisdom.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATONS'] = False
-
-    db.init(app)
-
-    with app.app_contect():
-        db.create_all()
-
-    return app
+if __name__ == '__main__':
+    app.run(debug=True)
