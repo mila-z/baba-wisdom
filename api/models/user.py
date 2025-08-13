96b425e09ec0a865c24918fc19c45ec5ab2a51ae
mila-z <m_zaprianova@protonmail.com>
2025-08-13 15:08:40 +0300

    fix: fix relationships between user and baba and apprentice, fix to_dict functions and apprentice class inheritance

--- api/models/user.py
+++ api/models/user.py
@@ -9,9 +9,13 @@ class User(db.Model):
     # role -> 'admin', 'baba', 'apprentice'
     role = db.Column(db.String(20), nullable=False)
 
+    # relationship setup
+    baba = db.relationship('Baba', backpopulates='user')
+
     def to_dict(self):
         return {
             'id': self.id,
+            'email': self.email,
             'username': self.username,
             'role': self.role,
             'type': self.type
@@ -20,29 +24,35 @@ class User(db.Model):
 
 class Baba(db.Model):
     id = db.Column(db.Integer, primary_key = True, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False, unique=True)
     village = db.Column(db.String(100), nullable=True)
     bio = db.Column(db.Text, nullable=True)
 
-    # foreign keys
+    # relationship setup
+    user = db.relationship('User', back_populates='baba')
     # each baba has many wisdom
     wisdoms = db.relationship('Wisdom', back_populates='baba')
-    baba_id = db.relationship('User', back_populates='baba')
 
     def to_dict(self):
-        data = super().to_dict()
-        data['village'] = self.village
-        data['bio'] = self.bio
-        return data
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'village': self.vilagte,
+            'bio': self.bio
+        }
 
-class Apprentice(User):
+class Apprentice(db.Model):
     birth_date = db.Column(db.Date, nullable=True)
-    
-    # foreign keys
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False, unique=True)
+
+    # relationship setup
+    user = db.relationship('User', back_populates = 'apprentice')
     # each apprentice has many reviews
     reviews = db.relationship('Review', back_populates='apprentice')
-    apprentice_id = db.relationship('User', back_populates = 'apprentice')
 
     def to_dict(self):
-        data = super().to_dict()
-        data['birth_date'] = self.birth_date.isoformat() if self.birth_date else None
-        return data
+        return {
+            'id': self.id,
+            'user_id': self.iser_id,
+            'birth_date': self.birth_date.isoformat() if self.birth_date else None
+        }
