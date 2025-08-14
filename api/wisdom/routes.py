5cf30ef845b8d8adbc6c0e237ea65c9703b7dec2
mila-z <m_zaprianova@protonmail.com>
2025-08-14 23:53:57 +0300

    fix: make posting, deleting and viewing accessible only to babas

--- api/wisdom/routes.py
+++ api/wisdom/routes.py
@@ -1,6 +1,6 @@
 from api.wisdom import bp
 from api.models import Wisdom
-from flask import jsonify, render_template, request, flash
+from flask import jsonify, render_template, request, flash, redirect, url_for
 from flask_login import current_user, login_required
 import json
 from api import db
@@ -33,12 +33,18 @@ def daily_wisdom():
 @login_required
 def post_wisdom():
     if request.method == 'POST':
+        if current_user.role != 'baba':
+            flash('Only a baba can post wisdom!', category='errror')
+            return redirect(url_for('wisdom.post_wisdom'))
+        
         wisdom_text = request.form.get('wisdom_text')
         age_input = request.form.get('age_restriction')
-        age_restriction = int(age_input) if age_input and age_input.isdigit() else 0
+        age_restriction = int(age_input) if age_input and age_input.isdigit() else 0 #maybe make an exception if its not digits
 
         if len(wisdom_text) < 1:
             flash('Note is too short!', category='error')
+        elif age_restriction < 0 or age_restriction > 122:
+            flash('Age must be between 0 and 122!', category='error')
         else:
             new_wisdom = Wisdom(
                 text=wisdom_text, 
@@ -54,17 +60,25 @@ def post_wisdom():
 @bp.route('/view-wisdom', methods=['GET', 'POST'])
 @login_required
 def view_wisdom():
+    if current_user.role != 'baba':
+        flash('Only a baba can view the wisdom she has posted!', category='error')
+        return redirect(url_for('wisdom.post_wisdom'))
+
     wisdom = Wisdom.query.filter_by(baba_id=current_user.baba.id).all()
     return render_template('view_wisdom.html', user=current_user, wisdoms=wisdom)
 
 @bp.route('/delete-wisdom', methods=['DELETE'])
 @login_required
 def delete_wisdom():
-    data = request.get_json()
-    wisdom_id = data.get('wisdom')
+    if current_user.role != 'baba':
+        flash('Only a baba can delete a wisdom she has posted!', category='error')
+        return redirect(url_for('wisdom.delete_wisdom'))
 
-    wisdom = Wisdom.quesry.get(wisdom_id)
-    if wisdom and wisdom.baba_id == current_user.id:
+    data = request.get_json()
+    wisdom_id = data.get('wisdomId')
+
+    wisdom = Wisdom.query.get(wisdom_id)
+    if wisdom and wisdom.baba_id == current_user.baba.id:
         db.session.delete(wisdom)
         db.session.commit()
 
