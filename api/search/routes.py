16acd678cda2f38357b9b07510a6a7093acc0a33
mila-z <m_zaprianova@protonmail.com>
2025-08-15 22:34:57 +0300

    feat: build on search functionality

--- api/search/routes.py
+++ api/search/routes.py
@@ -1,7 +1,36 @@
 from api.search import bp
-from flask import render_template
-from flask_login import current_user
+from flask import render_template, request, flash, redirect, url_for
+from flask_login import current_user, login_required
+from api.models import Wisdom, Category
+from sqlalchemy import or_
 
-@bp.route('/search')
+@bp.route('/search', methods=['GET', 'POST'])
+@login_required
 def search():
-    return render_template('search_wisdom.html', user=current_user)
+    wisdom = []
+
+    if request.method == 'POST':
+        query = Wisdom.query.outerjoin(Wisdom.categories)
+
+        categories = request.form.get('categories')
+        keywords = request.form.get('keywords')
+        if categories is None and keywords is None:
+            flash('You must have at least one input!', category='error')
+            return redirect(url_for('search.search'))
+        
+        if categories:
+            categories = [c.strip().lower() for c in categories.split(',')]
+            query = query.filter(
+                or_(
+                    Category.name.in_(categories),
+                    Category.id.is_(None))
+            )
+        
+        if keywords:
+            keywords = [k.strip() for k in keywords.split(',')]
+            keyword_filters = [Wisdom.text.ilike(f"%{k}%") for k in keywords]
+            query = query.filter(or_(*keyword_filters))
+        
+        wisdom = query.distinct().all()
+
+    return render_template('search_wisdom.html', user=current_user, wisdoms=wisdom)
